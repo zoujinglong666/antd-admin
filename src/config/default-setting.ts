170e99f16b729c3e19a334ffe88b7ff9d5f7b9aa
zou <675350014@qq.com>
2023-11-22 23:02:11 +0800

    first commit

---
+++ src/config/default-setting.ts
@@ -0,0 +1,53 @@
+import type { LayoutSetting } from '~@/stores/app'
+
+export default {
+  'title': 'Antdv Pro',
+  'theme': 'inverted',
+  'logo': '/logo.svg',
+  'collapsed': false,
+  'drawerVisible': true,
+  'colorPrimary': '#1677FF',
+  'layout': 'top',
+  'contentWidth': 'Fixed',
+  'fixedHeader': false,
+  'fixedSider': true,
+  'splitMenus': false,
+  'header': true,
+  'menu': true,
+  'watermark': false,
+  'menuHeader': true,
+  'footer': false,
+  'colorWeak': false,
+  'multiTab': true,
+  'multiTabFixed': false,
+  'keepAlive': true,
+  'accordionMode': true,
+  'leftCollapsed': true,
+  'headerHeight': 48,
+  'copyright': 'Antdv Pro Team 2023',
+  'animationName': 'slide-fadein-right',
+} as LayoutSetting
+
+export const animationNameList = [
+  {
+    label: 'None',
+    value: 'none',
+  },
+  {
+    label: 'Fadein Up',
+    value: 'slide-fadein-up',
+  },
+  {
+    label: 'Fadein Right',
+    value: 'slide-fadein-right',
+  },
+  {
+    label: 'Zoom Fadein',
+    value: 'zoom-fadein',
+  },
+  {
+    label: 'Fadein',
+    value: 'fadein',
+  },
+]
+export type AnimationNameValueType = 'none' | 'slide-fadein-up' | 'slide-fadein-right' | 'zoom-fadein' | 'fadein'
---
+++ src/pages/common/iframe.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const route = useRoute()
+const url = computed(() => route?.meta?.url)
+const loading = ref(true)
+function finishLoading() {
+  loading.value = false
+}
+</script>
+
+<template>
+  <iframe w-full h-full flex flex-col flex-1 :src="url" style="border: none;width: 100%;height: 100vh" @load="finishLoading" />
+</template>
+
+<style>
+
+</style>
